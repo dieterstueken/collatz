00a5a7b66db3c76716567644282728fb254fd5ad
dieterstueken <dieter.stueken@t-online.de>
2020-03-09 11:05:52 +0100

    docs

--- docs/collatz-tables.js
+++ docs/collatz-tables.js
@@ -1,155 +1,56 @@
-/*
- *  A tree of connected nodes
- *
- *  Each node has an index >= 0
- *  and an odd value of n = 2*index+1
- *  and a step towards its successor.
- *
- *  A succ always exists.
- *  pred and sib are calculated on demand.
- *
- *  relations are:
- *
- *  (2 * succ.index +1) * 2**step == 3 * index + 2;
- *
- *  sib.index = 4 * index + 2;
- *
- */
+function table3() {
 
-function Tree() {
+  var table = document.getElementById("table3");
 
-    function Node(succ, step) {
-        this.step = step;
-        this.max = this;
+  function row(k) {
+    var row = table.insertRow();
 
-        if(succ) {
-            this.succ = succ;
-            this.len = succ.len+1;
-            this.index = ((2*succ.index+1) * (1<<step) - 2) / 3;
-            this.path = succ.path.concat(".").concat(step);
-            this.sum = succ.sum + step;
-            if(succ.max.index>this.index)
-                this.max = succ.max;
-            this._fac = undefined;
-        } else {
-            // root node(0)
-            this.succ = this;
-            this._pred = this;
-            this.len = 1;
-            this.index = 0;
-            this.path = "1";
-            this.sum = 1;
-            this._fac = 1;
-        }
-
-        this.n = 2*this.index+1;
-
-        this.pred = function(i) {
-
-            var mod = this.index%3;
-
-            if(mod==1)
-                return undefined;
-
-            if(i>0)
-                return this.pred(i-1).sibl();
-
-            var pred = this._pred;
-            if(!pred) {
-                var step = mod ? 0 : 1;
-                pred = new Node(this, step);
-                this._pred = pred;
-            }
-
-            return pred;
-        }
-
-        this.sibl = function() {
-            var sibl = this._sibl;
-            if(!sibl) {
-                sibl = new Node(this.succ, this.step+2);
-                this._sibl = sibl;
-            }
-            return sibl;
-        }
-
-        this.fac = function() {
-            var fac = this._fac;
-            if(!fac) {
-                fac = 1;
-                for(i=1; i<this.index; i++) {
-                    if(this.contains(i))
-                        ++fac;
-                }
-                this._fac = fac;
-            }
-            return fac;
-        }
-
-        this.contains = function(i) {
-            i = 2*i+1;
-            do {
-
-                if(i==this.n)
-                    return true;
-
-                i = (3*i+1)/2;
-                while((i%2)==0)
-                    i = i/2;
-
-            } while(i>1)
-
-            return false;
-        }
-
-        this.succeeds = function(node) {
-            if(node==undefined)
-                return false;
-
-            if(node==this)
-                return true;
-
-            if(this.succ==this)
-                return false;
-
-            return this.succ.succeeds(node);
-        }
-
-        this.toString = function() {
-            return this.n.toString().concat('(').concat(this.len).concat(':').concat(this.path).concat(')');
-        }
+    function cell(value) {
+      var cell=row.insertCell()
+      //var span =  document.createElement("span");
+      //cell.appendChild(span);
+      cell.textContent=value;
+      return cell;
     }
 
-    this.root = new Node(undefined, 1);
+    var kn = 3*k+2;
+    var l=0;
 
-    this.node = function(index) {
-
-        if(index==0)
-            return this.root;
-
-        var k = 3*index + 2;
-        var step = 0;
-        while(k>1 && !(k%2)) {
-            ++step;
-            k>>=1;
-        }
-
-        // to index
-        k = (k-1)/2;
-
-        step >>= 1;
-
-        return this.node(k).pred(step);
+    while(kn%2==0) {
+      kn /= 2;
+      ++l;
     }
 
-    this.inode = function(index) {
+    kn -= 1;
+    kn /= 2;
 
-        if(index == undefined)
-            return null;
+    cell(2*k+1).style.textAlign = "right";
+    cell(k).style.textAlign = "right";
+    cell(kn).style.textAlign = "right";
+    cell(seq(k)).style.textAlign = "left";
+  }
 
-        if(index%2!=1)  // even
-            return null;
-            
-        return this.node((index-1)/2);
+  function seq(k) {
+    if(k<1)
+        return "";
+
+    k = 3*k+2;
+
+    var l=0;
+    while(k%2==0) {
+        k /= 2;
+        ++l
     }
-}
+
+     k -= 1;
+     k /= 2;
+
+    return l.toString().concat(' ').concat(seq(k));
+  }
+
+  for (var k = 0; k < 8; k++) {
+    row(k);
+  }
+};
+
+window.onload = table3;
