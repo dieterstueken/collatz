d6f059d4feb6b5d45da652ec7a8b8bbd5bb7fe07
dieterstueken <dieter.stueken@t-online.de>
2020-03-29 12:21:44 +0200

    index

--- docs/collatz-tables.js
+++ docs/collatz-tables.js
@@ -1,56 +1,245 @@
-function table3() {
+function L(n) {
+    let L=0;
+    while(n%2===0) {
+        ++L;
+        n /= 2;
+    }
 
-  var table = document.getElementById("table3");
+    return L;
+}
+
+function table1(n) {
+
+  let table = document.getElementById("table1");
+
+  function row(n) {
+
+      let row = table.insertRow();
+
+      function cell(value) {
+          let c=row.insertCell();
+          c.style.textAlign = "right";
+
+          if(value != undefined)
+              c.textContent=value;
+          return c;
+      }
+
+      let m=3*n+1;
+      let l = L(m);
+      let next = m>>l;
+
+      cell(n);
+      cell(m);
+      cell(n.toString(2));
+      
+      let del = document.createElement("del")
+      for(let i=0; i<l; ++i)
+        del.append("0");
+
+      cell().append(next.toString(2), del);
+
+      cell(l-1);
+
+      return next;
+  }
+
+  while(n>1) {
+    n = row(n);
+  }
+
+  row(1);
+}
+
+function table2(k) {
+
+  let table = document.getElementById("table2");
 
   function row(k) {
-    var row = table.insertRow();
+
+      let row = table.insertRow();
+
+      function cell(value) {
+          let c=row.insertCell();
+          c.style.textAlign = "right";
+
+          if(value != undefined)
+              c.textContent=value;
+          return c;
+      }
+
+      let n=2*k+1;
+
+      let m=3*k+2;
+      let l = L(m);
+
+      let next = m>>(l+1);
+
+      cell(n);
+      cell(k);
+      cell(m);
+      cell(k.toString(2));
+
+      let del = document.createElement("del")
+      del.append("1");
+
+      for(let i=0; i<l; ++i)
+        del.append("0");
+
+      cell().append(next>0?next.toString(2):"", del);
+
+      cell(l);
+
+      return next;
+  }
+
+  while(k>0) {
+    k = row(k);
+  }
+
+  row(0);
+}
+
+function table3(N) {
+
+  let table = document.getElementById("table3");
+
+  function row(k) {
+    let row = table.insertRow();
 
     function cell(value) {
-      var cell=row.insertCell()
-      //var span =  document.createElement("span");
-      //cell.appendChild(span);
-      cell.textContent=value;
-      return cell;
+      let c=row.insertCell();
+      c.style.textAlign = "right";
+
+      if(value != undefined)
+          c.textContent=value;
+      return c;
     }
 
-    var kn = 3*k+2;
-    var l=0;
+    let kn = 3*k+2;
+    let l=L(kn);
+    kn >>= l+1;
 
-    while(kn%2===0) {
-      kn /= 2;
-      ++l;
-    }
+    let c = cell(k);
 
-    kn -= 1;
-    kn /= 2;
-
-    cell(2*k+1).style.textAlign = "right";
-    cell(k).style.textAlign = "right";
-    cell(kn).style.textAlign = "right";
-    cell(seq(k)).style.textAlign = "left";
+    c = cell(kn);
+    seq(cell(), k);
   }
 
-  function seq(k) {
-    if(k<1)
-        return "";
+  function seq(cell, k) {
 
-    k = 3*k+2;
-
-    var l=0;
-    while(k%2===0) {
-        k /= 2;
-        ++l
+    function span(l, k) {
+        let span =  document.createElement("span");
+        span.textContent = l.toString();
+        span.title = k.toString();
+        cell.append(span);
     }
 
-     k -= 1;
-     k /= 2;
+    if(k<1) {
+        span(1,0);
+    } else {
 
-    return l.toString().concat(' ').concat(seq(k));
+        k = 3*k+2;
+        let l=L(k);
+        k >>= l+1;
+
+        span(l,k);
+        if(k>0) {
+          cell.append(' ');
+          seq(cell, k);
+        }
+    }
   }
 
-  for (var k = 0; k < 8; k++) {
+  for (let k = 0; k < N; k++) {
     row(k);
   }
 }
 
-window.onload = table3;
+
+function table4(N) {
+
+  let table = document.getElementById("table4");
+
+  function row(k) {
+    let row = table.insertRow();
+
+      function cell(value) {
+          let c=row.insertCell();
+          c.style.textAlign = "right";
+
+          if(value != undefined)
+              c.textContent=value;
+          return c;
+      }
+
+    let kn = 3*k+2;
+    let l=L(kn);
+    kn >>= l+1;
+
+    let c = cell(k);
+
+    switch(k%3) {
+      case 0:
+        c.style.color = "blue";
+        break;
+      case 2:
+         c.style.color = "green";
+         break;
+    }
+
+    c = cell(kn);
+    if(kn>k)
+      c.style.color = "red";   
+
+    c = cell();
+    c.style.textAlign = "left";
+    seq(c, k);
+  }
+
+  function seq(cell, k) {
+
+    function span(l, k, kn) {
+        let span =  document.createElement("span");
+        span.textContent = l.toString();
+        span.title = k.toString();
+        switch(k%3) {
+            case 0:
+                span.style.color = "blue";
+                break;
+            case 2:
+                 span.style.color = "green";
+                 break;
+        }
+        cell.append(span);
+    }
+
+    if(k<1) {
+        span(1,0,0);
+    } else {
+
+        let kn = 3*k+2;
+        let l=L(kn);
+        kn >>= l+1;
+
+        if(kn>0) {
+          seq(cell, kn);
+          cell.append(' ');
+        }
+
+        span(l,k, kn);
+    }
+  }
+
+  for (let k = 0; k < N; k++) {
+    row(k);
+  }
+}
+
+window.onload = function() {
+table1(9);
+table2(4);
+table3(10);
+table4(40);
+}
+
