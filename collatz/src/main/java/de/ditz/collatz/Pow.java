adcb424b6c13c68aa649d0ed80266a228277439c
d.stueken <d.stueken@conterra.de>
2025-02-01 13:37:04 +0100

    pow3

--- collatz/src/main/java/de/ditz/collatz/Pow.java
+++ collatz/src/main/java/de/ditz/collatz/Pow.java
@@ -4,17 +4,29 @@ import java.math.BigInteger;
 
 public class Pow {
 
+    static final BigInteger I1 = BigInteger.valueOf(1);
     static final BigInteger I3 = BigInteger.valueOf(3);
 
     public static void main(String[] args) {
+
+        Integer hit = args.length>0 ? Integer.parseInt(args[0]) : null;
+
+        System.out.print("    ");
+        for(int k=10; k<72; k += 10) {
+            System.out.format("%10d", k);
+        }
+        System.out.println();
+        
         BigInteger p3 = BigInteger.valueOf(1);
         for(int i=1; i<33; ++i) {
-            BigInteger p3k = p3.subtract(BigInteger.valueOf(1));
+            BigInteger p3k = p3.subtract(I1);
             System.out.format("%2d ", i);
             for(int k=1; k<72; ++k) {
                 int l = p3k.getLowestSetBit();
                 if(k%2==1) {
-                    if (l > 1)
+                    if(hit!=null && l!=hit)
+                        System.out.print("  ");
+                    else if (l > 1)
                         System.out.format("%2X", l);
                     else
                         System.out.print("  ");
