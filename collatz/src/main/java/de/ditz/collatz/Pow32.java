c8fa01a0689655442089fbdc14e94d35d0b498f8
d.stueken <d.stueken@conterra.de>
2025-02-11 17:08:08 +0100

    Pow32

--- collatz/src/main/java/de/ditz/collatz/Pow32.java
+++ collatz/src/main/java/de/ditz/collatz/Pow32.java
@@ -8,7 +8,7 @@ import java.math.BigInteger;
  * Date: 02.02.25
  * Time: 19:03
  */
-public class Pow3 {
+public class Pow32 {
 
     public static BigInteger I0 = BigInteger.valueOf(0);
     public static BigInteger I3 = BigInteger.valueOf(3);
@@ -17,25 +17,23 @@ public class Pow3 {
         BigInteger p3 = I3;
 
         for(int i=1; i<100; ++i) {
-
             int len = p3.bitLength();
-            StringBuilder binary = binary(p3);
-            System.out.format("%3d %3d   %s\n", i, len, binary);
-            p3 = p3.multiply(I3);
+            StringBuilder binary = binary(p3.shiftRight(i));
+            binary.delete(0, i);
+            System.out.format("%3d %,15d %s\n", i, len, binary);
+            p3 = p3.parallelMultiply(p3);
         }
     }
 
-    private static StringBuilder binary(BigInteger p3, String digits) {
+    private static StringBuilder binary(BigInteger p3) {
 
         StringBuilder sb = new StringBuilder();
 
-        int l = digits.length();
-        
-        while(!I0.equals(p3))  {
+        while(!I0.equals(p3) && sb.length()<100)  {
             int l = p3.getLowestSetBit();
 
             for(int i=0; i<l; ++i)
-                sb.append('.');
+                sb.append(' ');
 
             sb.append('1');
             p3 = p3.shiftRight(l+1);
