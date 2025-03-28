b6cc778443be232c98f070afe6e154b5088f74a8
d.stueken <d.stueken@conterra.de>
2025-03-28 13:48:35 +0100

    r = 1/k

--- collatz/src/main/java/de/ditz/draw/CollatzDiagram1.java
+++ collatz/src/main/java/de/ditz/draw/CollatzDiagram1.java
@@ -9,7 +9,7 @@ import java.awt.*;
  * Date: 30.06.24
  * Time: 15:58
  */
-public class CollatzDiagram extends LabeledPane {
+public class CollatzDiagram1 extends LabeledPane {
 
     static final double L32 = 1.0/Math.log(1.5);
 
@@ -31,17 +31,57 @@ public class CollatzDiagram extends LabeledPane {
         final double yl = scales.sy.lower();
         final double yh = scales.sy.upper();
 
-        for(long k = Math.max(1L, (long)Math.ceil(yl)); k<yh; ++k) {
-            int iy = scales.sy.pix(k);
+        // k = p15(t)
+        // y = 1/k
+
+        if(yl>=1 || yh<=0)
+            return;
+
+        // pixel drawn per scanline
+        int lx = 3*scales.sx.len();
+        int mx = 0;
+
+        // top down
+        long k = Math.max(1, (long) Math.ceil(1/yh));
+        double y = 1.0/k;
+        int iy = scales.sy.pix(y);
+
+        while(y>yl) {
 
             double t = l15(k);
             t += (int)Math.ceil(xl-t);
-            
+
             for(; t<=xh; t += 1.0) {
                 int ix = scales.sx.pix(t);
                 g.fillRect(ix, iy-1, 3, 3);
+                ++mx;
             }
+
+            y = 1.0/++k;
+            int ny = scales.sy.pix(y);
+
+            if(ny==iy) {
+                // same scanline
+                if (mx > lx) {
+
+                    // skip ahead
+                    ny = iy + 1;
+                    y = scales.sy.val(ny);
+                    if(y<0)
+                        break;
+
+                    long kn = (long) Math.ceil(1/y);
+                    if (kn == k)
+                        throw new IllegalStateException("k unchanged");
+                    k = kn;
+                }
+            } else {
+                mx=0;
+            }
+            iy = ny;
         }
+
+        mx = 0; // dummy
     }
 
     static void open() {
@@ -50,7 +90,7 @@ public class CollatzDiagram extends LabeledPane {
         p.setPreferredSize(new Dimension(300, 150));
         p.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 
-        p.add(new CollatzDiagram());
+        p.add(new CollatzDiagram1());
 
         JFrame frame = new JFrame("Paint");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -61,6 +101,6 @@ public class CollatzDiagram extends LabeledPane {
     }
 
     public static void main(String ... args) {
-        SwingUtilities.invokeLater(CollatzDiagram::open);
+        SwingUtilities.invokeLater(CollatzDiagram1::open);
     }
 }
